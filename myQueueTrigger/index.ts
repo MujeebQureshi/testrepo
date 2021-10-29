35c8e39d54c915def9407df79b644aac74a2243e
MujeebQureshi <abdul.mujeeb.qureshi@hotmail.com>
2021-10-30 01:31:15 +0500

    first commit 2

--- myQueueTrigger/index.ts
+++ myQueueTrigger/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context } from "@azure/functions"
 
 const queueTrigger: AzureFunction = async function (context: Context, myQueueItem: string): Promise<void> {
-    context.log('Queue trigger function processed work item', myQueueItem);
+    context.log('Queue trigger function processed work items', myQueueItem);
 };
 
 export default queueTrigger;
